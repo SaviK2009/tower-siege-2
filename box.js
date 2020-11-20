4425fd27cff2684110f591caad7f8c7346c93f14
Savi <68930347+SaviK2009@users.noreply.github.com>
2020-11-20 10:57:52 -0600

    Add files via upload

--- box.js
+++ box.js
@@ -14,11 +14,16 @@ class Box {
     }
     display(){
       var pos =this.body.position;
-      rectMode(CENTER);
-      push();
-      rect(pos.x, pos.y, this.width, this.height);
-      pop();
+      
+      if(this.body.speed<3){
+        rectMode(CENTER)
+        rect(pos.x, pos.y,this.width, this.height)
+      }
+      else{
+        World.remove(world,this.body)
+        push();
+        this.visibility=this.visibility-5
+        pop();
     }
-  };
-  
- 
+    };
+  }
